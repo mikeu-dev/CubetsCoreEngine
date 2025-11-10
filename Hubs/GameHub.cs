f382a0e4e43151128ca44ec33ae85f7189a36b59
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-10 19:50:22 +0700

    this is GameHub class. SignalR Hub for real-time

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using cubets_core.Modules.GameState.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace cubets_core.Hubs
+{
+    public class GameHub: Hub
+    {
+        public async Task SendPlayerState(PlayerState state)
+        {
+            await Clients.Others.SendAsync("ReceivePlayerState", state);
+        }
+    }
+}
