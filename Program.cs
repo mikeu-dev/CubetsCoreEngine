e43d4dbdff9c8e1cbf369e519967b6a4cebc38f6
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-10 19:52:40 +0700

    program class has modified. add services for SignalR and SroceService

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
+using cubets_core.Hubs;
+using cubets_core.Modules.Score.Services;
 using MySqlConnector;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("ConnectionStrings")!);
 
-// Add services to the container.
+builder.Services.AddSignalR();
+
+builder.Services.AddScoped<ScoreService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -23,5 +27,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapHub<GameHub>("/gamehub");
 app.Run();
