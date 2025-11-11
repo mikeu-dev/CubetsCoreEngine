b3a81dbb32f70d82e89670d88216b9e49e43c9f7
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-11 15:48:42 +0700

    Player module

---
+++ Modules/Player/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using cubets_core.Modules.Auth.Models;
+
+namespace cubets_core.Modules.Player.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        // foreign key ke User
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+
+        public string Nickname { get; set; } = string.Empty;
+        public int Level { get; set; } = 1;
+        public int Coins { get; set; } = 0;
+        public int HighScore { get; set; }
+        public int LowScore { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Modules/Player/Services/PlayerService.cs
@@ -0,0 +1,22 @@
+﻿using cubets_core.Data;
+using cubets_core.Modules.Player.DTOs;
+
+namespace cubets_core.Modules.Player.Services
+{
+    public class PlayerService
+    {
+        private readonly CubetsDbContext _dbContext;
+        public PlayerService(CubetsDbContext dbContext) => _dbContext = dbContext;
+
+        public PlayerDTO? GetPlayerDto(int Id)
+        {
+            var player = _dbContext.Players.Find(Id);
+            if (player == null) return null!;
+            return new PlayerDTO {
+                Name = player.Nickname, 
+                Highscore = player.HighScore, 
+                Lowscore = player.LowScore
+            };
+        }
+    }
+}
