e8e89e6c1e1aa879aa17f8054b20b055f8e3e5c5
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-11 15:46:01 +0700

    modify data db context

--- Data/CubetsDbContext.cs
+++ Data/CubetsDbContext.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using cubets_core.Modules.Auth.Models;
 using cubets_core.Modules.GameState.Models;
 using cubets_core.Modules.Score.Models;
+using cubets_core.Modules.Player.Models;
+using cubets_core.Modules.Auth.Models;
 
 namespace cubets_core.Data
 {
@@ -10,8 +11,21 @@ namespace cubets_core.Data
         public CubetsDbContext(DbContextOptions<CubetsDbContext> options) : base(options)
         {
         }
+
+        public DbSet<User> Users { get; set; }
         public DbSet<Player> Players { get; set; }
         public DbSet<PlayerState> PlayerStates { get; set; }
         public DbSet<ScoreEntry> ScoreEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // konfigurasi relasi atau constraint khusus di sini
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Player)
+                .WithOne(p => p.User)
+                .HasForeignKey<Player>(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
