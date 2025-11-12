00def34e5d210dfcbf03be71da9ef13d2961b0ee
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-12 15:57:24 +0700

    Interface for score service

---
+++ Modules/Score/Services/IScoreService.cs
@@ -0,0 +1,9 @@
+﻿using cubets_core.Modules.Score.DTOs;
+
+namespace cubets_core.Modules.Score.Services
+{
+    public interface IScoreService
+    {
+        Task<List<ScoreEntryDto>> GetTopScoresAsync(int playerId);
+    }
+}
